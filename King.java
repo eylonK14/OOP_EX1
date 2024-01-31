8c81cdd7dddb562a75bb8dce418cfd522d7d231a
eylonK14 <116713250+eylonK14@users.noreply.github.com>
2024-01-31 15:50:40 +0200

    Update King.java
    changed king's unicode

--- King.java
+++ King.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 public class King extends ConcretePiece {
     public King(ConcretePlayer player, Position pos) {
-        super(player, "K7", pos, "♔");
+        super(player, "K7", pos, "\u2654");
     }
 }
